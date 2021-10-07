892874c217829644fe803a6a7e157a51b0cb4ff6
Desi-J <jonesdesiree@ymail.com>
2021-10-07 13:31:04 -0700

    mobile styles done

--- js/main.js
+++ js/main.js
@@ -3,34 +3,16 @@ console.log("working")
 const menuBtn = document.querySelector(".menu")
 exitBtn = document.querySelector(".exit")
 nav = document.querySelector("nav");
-links = document.querySelectorAll(".link");
 
-console.log(menuBtn, exitBtn, nav, links);
-
-function openNav() {
-    menuBtn.classList.add("menu-btn");
-    // nav.classList.remove("hidden");
-}
-
-function closeNav() {
-    menuBtn.classList.remove("hidden");
-    nav.classList.add("hidden");    
-}
+console.log(menuBtn, exitBtn, nav);
 
 
 menuBtn.addEventListener("click", () => {
-    openNav()
+   nav.classList.add("menu-btn");
 });
 
 exitBtn.addEventListener("click", () => {
-        console.log("working")
-        closeNav()
-    })
-
-    links.forEach(link => {
-        link.addEventListener("click", () => {
-            closeNav()
-        })
+   nav.classList.remove("menu-btn");    
     })
 
 
